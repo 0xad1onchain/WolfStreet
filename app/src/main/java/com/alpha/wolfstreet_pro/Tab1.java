62c806bad7e9e7381c40dafaef74680b9e4f53d4
Aditya Gupta <adi.gupta13@gmail.com>
2017-03-13 13:08:35 +0530

    Initial commit

---
+++ app/src/main/java/com/alpha/wolfstreet_pro/Tab1.java
@@ -0,0 +1,21 @@
+package com.alpha.wolfstreet_pro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Adi on 13/03/17.
+ */
+
+public class Tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/alpha/wolfstreet_pro/Tab2.java
@@ -0,0 +1,21 @@
+package com.alpha.wolfstreet_pro;
+
+/**
+ * Created by Adi on 13/03/17.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+}
